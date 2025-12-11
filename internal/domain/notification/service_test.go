926fca987a644004bc5776e41c0e75abe6ca39b2
Miguel Ramos <return.infox@gmail.com>
2025-12-11 12:02:13 -0300

    codigo actualizado

--- internal/domain/notification/service_test.go
+++ internal/domain/notification/service_test.go
@@ -1,11 +1,29 @@
 package notification
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 /*
 Test[Metodo]_[Escenario]_[ResultadoEsperado]
 */
 
 func TestNotify_ValidInput_Success(t *testing.T) {
+	// Arrange (Preparar)
+	mockMessenger := new(MockMessenger)
 
+	// Configurar la expectativa
+	// 	mockMessenger.On("metodo", arg1, arg2) retorno
+	mockMessenger.On("Send", "user@testing.com", "Hola desde Testing").Return(nil)
+
+	service := NewNotificationService(mockMessenger)
+	// Act
+
+	err := service.Notify("user@testing.com", "Hola desde Testing")
+	// Asserts
+	assert.NoError(t, err)
+
+	mockMessenger.AssertExpectations(t)
 }
