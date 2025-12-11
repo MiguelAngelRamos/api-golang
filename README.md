5d58f7283971801743f01bf497ab91fb90243926
Miguel Ramos <return.infox@gmail.com>
2025-12-11 11:32:13 -0300

    codigo de testing

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dependencia de Testing a instalar
+
+```sh
+go get github.com/stretchr/testify
+```
---
+++ internal/domain/notification/mock_messeger_test.go
@@ -0,0 +1,7 @@
+package notification
+
+import "github.com/stretchr/testify/mock"
+
+type MockMessenger struct {
+	mock.Mock
+}
