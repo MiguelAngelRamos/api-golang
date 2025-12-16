9d32993eece75f3118abb2ccfa3f985e6a254ff2
Miguel Ramos <return.infox@gmail.com>
2025-12-16 10:09:57 -0300

    comentarios

--- internal/domain/notification/mock_messeger_test.go
+++ internal/domain/notification/mock_messeger_test.go
@@ -18,5 +18,34 @@ Nos permite
 func (mockStruct *MockMessenger) Send(destination string, message string) error {
 	// argumentos
 	args := mockStruct.Called(destination, message)
+	/*
+		args := mockStruct.Called(destination, message)
+		En este momento:
+		Testify registra: "Se llamo Send con {'user@example.com, "Hola desde Testing"}"
+		Testify busca en las expectativas configuradas en con .On()
+		- Encuentra .On("Send", "user@testing.com", "Hola desde Testing").Return(nil)
+		- args ahora contiene un: mock.Arguments{nil}
+	*/
+
+	/*
+		mockStruct.Called() ?
+		1. Registra la llamada al metodo "Send" con los argumentos recibidos
+
+		2. Busca expectativas configuradas con el .On()
+
+		3. Devuelve un objeto mock.Arguments con los valores configurados con .Return()
+
+	*/
+
+	/*
+	 Ejemplo de lo que hace "mockStruct.Called"
+	  args = mock.Arguments{nil}
+	*/
 	return args.Error(0)
+	/*
+		- args.Error(0)  accede a la posicion 0 del slice
+		  args[0] = nil
+			Convierte a nil a tipo error
+			Devuelve nil (nil significa que no hay error)
+	*/
 }
--- internal/domain/notification/service_test.go
+++ internal/domain/notification/service_test.go
@@ -16,7 +16,11 @@ func TestNotify_ValidInput_Success(t *testing.T) {
 
 	// Configurar la expectativa
 	// 	mockMessenger.On("metodo", arg1, arg2) retorno
+	// El on genera expectativa, cuando se llame a send con estos 2 arguments
 	mockMessenger.On("Send", "user@testing.com", "Hola desde Testing").Return(nil)
+	/*nil significa que no hay error
+	mock.On(..).Return(nil) configuramos que el mock no devuelva un error
+	*/
 
 	service := NewNotificationService(mockMessenger)
 	// Act
@@ -26,4 +30,29 @@ func TestNotify_ValidInput_Success(t *testing.T) {
 	assert.NoError(t, err)
 
 	mockMessenger.AssertExpectations(t)
+	/*AssertExpectations
+	  verifica qeu se llamo a "Send" con {"user@testing.com","Hola desde Testing" }
+	*/
+}
+
+func TestNotify_EmptyDestination_ReturnsError(t *testing.T) {
+	mockMessenger := new(MockMessenger)
+	service := NewNotificationService(mockMessenger)
+	err := service.Notify("", "mensaje válido")
+
+	/*
+		err: valor actual retornado por service.Notify "ErrEmptyDestination"
+		t: Objeto de test
+	*/
+	assert.Error(t, err) /* En esta linea recien nuestro t conoce el error*/
+
+	assert.Equal(t, ErrEmptyDestination, err)
+	/*
+		ErrEmptyDestination : valor esperado
+		err: valor que realmente recibo el error generado la realidad
+		testing.T
+	*/
+
+	mockMessenger.AssertNotCalled(t, "Send")
+
 }
